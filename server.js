cbe9b2453be3ebe5520a1759e8d04c726591fa04
Denisse 'Scar' Del Monte <d.delmonte@yahoo.com>
2022-10-14 03:13:56 -0400

    created MVC structure, add data form working, files added to db

--- server.js
+++ server.js
@@ -7,16 +7,15 @@ const express = require('express');
 const app = express();
 const methodOverride = require("method-override");
 const logger = require("morgan");
-// const mainRoutes = require("./routes/main");
-// const postRoutes = require("./routes/posts");
+const mainRoutes = require("./routes/main");
+const dataRoutes = require("./routes/data");
 // const commentRoutes = require("./routes/comments");
-// const PORT = 3000
 
 //Using EJS for views
 app.set('view engine', 'ejs')
 //Static Folder
 app.use(express.static('public'))
-app.use('/static', express.static('public'))
+// app.use('/static', express.static('public'))
 //Body Parsing
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -26,41 +25,27 @@ app.use(logger("dev"));
 app.use(methodOverride("_method"));
 
 //Setup Routes For Which The Server Is Listening
-// app.use("/", mainRoutes);
-// app.use("/data", postRoutes);
-// app.use("/post", commentRoutes);
+app.use("/", mainRoutes);
+app.use("/data", dataRoutes);
+// app.use("/feedback", mainRoutes);
 
-app.get('/',(req, res)=>{
-    res.render('index.ejs'
-    // , { info: data }
-    )
-    // .catch(error => console.error(error))
-})
 
-app.get('/data',(req, res)=>{
-//     db.collection('data').find().toArray()
-//     .then(data => {
-        res.render('data.ejs'
-        //, { info: data }) }
-        )
-//     .catch(error => console.error(error))
-})
-
-app.get('/add-data',(req, res)=>{
-    res.render('add-data.ejs')
-})
-
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'g-i'
-
-    MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
+// Connect to Database
+const connectDB = async () => {
+    try {
+    const conn = await mongoose.connect(process.env.DB_STRING, { 
+        useUnifiedTopology: true
     })
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+};
 
 
-app.listen(process.env.PORT || dotenv.PORT , ()=>{
-    console.log(`Server running on port ${dotenv.PORT}`)
-})
+connectDB();
+// Run Server 
+app.listen(process.env.PORT, () => {
+    console.log("Server is running, you better catch it!");
+});
